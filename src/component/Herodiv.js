66b509ee0f9538bcb68c5d3c38ce614432bcaa49
jay90194 <jay90194@gmail.comm>
2020-11-23 16:13:01 +0530

    This

--- src/component/Herodiv.js
+++ src/component/Herodiv.js
@@ -10,7 +10,7 @@ export default function Herodiv(props) {
     return (
       <div>
       <p className="absolute z-10 text-3xl p-5 font-sans ml-19 font-medium capitalize tracking-widest mt-48"> beauty without
-expression is boring
+expression is boring.
 </p>
       <Img className="h-129 w-full bg-cover" fluid={props.image} alt="none" />
       <h1 className="text-gray-800 p-4 text-center font-medium capitalize text-xl mb-5 lg:text-2xl" >Recent Blogs</h1>
